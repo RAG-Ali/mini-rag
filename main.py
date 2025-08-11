d89884776f4fa671b19644b533321b0c57a98775
Aly Khaled <aly1khaled@gmail.com>
2025-08-11 13:02:07 +0300

    end of tut

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 
+from dotenv import load_dotenv
+load_dotenv(".env")
+
+from routes import base
+
+
 app=FastAPI()
 
-
-@app.get("/welcome")
-def welcome():
-    return "Message : Hello World"
+app.include_router(base.base_router)
